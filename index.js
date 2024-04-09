758fb516d6139f291384303a8589bca2bb3c79ab
vaisberger <yaelw123@gmail.com>
2024-04-09 16:32:23 +0300

    9.4 1

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-const app={
-    pages:[],
+const dates = document.querySelectorAll('td');
+const app = {
+    pages: [],
     show: new Event('show'),
-    init:function(){
-        app.pages=document.querySelectorAll('.page');
-        app.pages.forEach((pg)=>{
-            pg.addEventListener('show',app.pageShown);
+    init: function () {
+        app.pages = document.querySelectorAll('.page');
+        app.pages.forEach((pg) => {
+            pg.addEventListener('show', app.pageShown);
         })
-        
-        document.querySelectorAll('.nav-link').forEach((link)=>{
-            link.addEventListener('click',app.nav)
+
+        document.querySelectorAll('.nav-link').forEach((link) => {
+            link.addEventListener('click', app.nav)
         })
+        history.replaceState({}, 'Home', '#home');
+        window.addEventListener('hashchange', app.poppin);
     },
-    nav: function(ev){
+    nav: function (ev) {
         ev.preventDefault();
-        let currentPage=ev.target.getAttribute('data-target');
+        let currentPage = ev.target.getAttribute('data-target');
         document.querySelector('.active').classList.remove('active');
         document.getElementById(currentPage).classList.add('active');
-        history.pushState({},currentPage,`#${currentPage}`);
+        console.log(currentPage);
+        history.pushState({}, currentPage, `#${currentPage}`);
+        document.getElementById(currentPage).dispatchEvent(app.show);
     },
-    pageShown: function(ev){
-
-    },
-    poppin: function(ev){
-
+    poppin: function (ev) {
+        console.log(location.hash, 'popstate event')
     }
 }
-document.addEventListener('DOMContentLoaded',app.init);
+document.addEventListener('DOMContentLoaded', app.init);
