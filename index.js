4cc89523aca7e6c4433e5304de099325768e7e45
vaisberger <yaelw123@gmail.com>
2024-04-09 17:45:53 +0300

    9.4

--- index.js
+++ index.js
@@ -2,29 +2,37 @@ const dates = document.querySelectorAll('td');
 const app = {
     pages: [],
     show: new Event('show'),
-    init: function () {
+    init: function(){
         app.pages = document.querySelectorAll('.page');
-        app.pages.forEach((pg) => {
+        app.pages.forEach((pg)=>{
             pg.addEventListener('show', app.pageShown);
         })
-
-        document.querySelectorAll('.nav-link').forEach((link) => {
-            link.addEventListener('click', app.nav)
+        
+        document.querySelectorAll('.nav-link').forEach((link)=>{
+            link.addEventListener('click', app.nav);
         })
         history.replaceState({}, 'Home', '#home');
-        window.addEventListener('hashchange', app.poppin);
+        window.addEventListener('popstate', app.poppin);
     },
-    nav: function (ev) {
+    nav: function(ev){
         ev.preventDefault();
         let currentPage = ev.target.getAttribute('data-target');
         document.querySelector('.active').classList.remove('active');
         document.getElementById(currentPage).classList.add('active');
-        console.log(currentPage);
+        console.log(currentPage)
         history.pushState({}, currentPage, `#${currentPage}`);
         document.getElementById(currentPage).dispatchEvent(app.show);
-    },
-    poppin: function (ev) {
-        console.log(location.hash, 'popstate event')
+    },    poppin: function(ev){
+        console.log(location.hash, 'popstate event');
+        let hash = location.hash.replace('#' ,'');
+        document.querySelector('.active').classList.remove('active');
+        document.getElementById(hash).classList.add('active');
+        console.log(hash)
+        document.getElementById(hash).dispatchEvent(app.show);
     }
 }
 document.addEventListener('DOMContentLoaded', app.init);
+document.getElementById("select").addEventListener('onchange',num);
+function num(){
+    alert('hi');
+}
