294f9fcea38d28fdb92ee9a2e93e2ee2effa61d7
vaisberger <yaelw123@gmail.com>
2024-04-16 20:17:33 +0300

    16.4

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const dates = document.querySelectorAll('.days');
 const app = {
     pages: [],
     show: new Event('show'),
-    init: function(){
+    init: function () {
         app.pages = document.querySelectorAll('.page');
-        app.pages.forEach((pg)=>{
+        app.pages.forEach((pg) => {
             pg.addEventListener('show', app.pageShown);
         })
-        
-        document.querySelectorAll('.nav-link').forEach((link)=>{
+
+        document.querySelectorAll('.nav-link').forEach((link) => {
             link.addEventListener('click', app.nav);
         })
         history.replaceState({}, 'Home', '#home');
         window.addEventListener('popstate', app.poppin);
     },
-    nav: function(ev){
+    nav: function (ev) {
         ev.preventDefault();
         let currentPage = ev.target.getAttribute('data-target');
         document.querySelector('.active').classList.remove('active');
@@ -22,29 +22,51 @@ const app = {
         console.log(currentPage)
         history.pushState({}, currentPage, `#${currentPage}`);
         document.getElementById(currentPage).dispatchEvent(app.show);
-    },    poppin: function(ev){
+    }, poppin: function (ev) {
         console.log(location.hash, 'popstate event');
-        let hash = location.hash.replace('#' ,'');
+        let hash = location.hash.replace('#', '');
         document.querySelector('.active').classList.remove('active');
         document.getElementById(hash).classList.add('active');
         console.log(hash)
         document.getElementById(hash).dispatchEvent(app.show);
     }
 }
-
+// a function that gets all meetings of specific user
+function getmeetings() {
+    let meetings = [];
+  
+    const fxhr = new FXHR();
+    fxhr.open("GET", "");
+    fxhr.send({ meeting: "meeting" }, (meetinglist) => {
+      meetings = meetinglist;
+    });
+    return meetings;
+  }
 function addMeeting() {
-    var formwindow= document.getElementsByClassName(newmeeting);
-    formwindow.classList.add('show');
+    document.getElementById("newmeeting").style.visibility = "visible";
 }
 function exit() {
-    var formwindow= document.getElementById(newmeeting);
-    formwindow.classList.add('show');
+    document.getElementById("newmeeting").style.visibility = "hidden";
+}
+let meetinglist =[];
+function add(){
+   const date =document.getElementById("date") ;
+   const time =document.getElementById("time") ;
+   const place =document.getElementById("place") ;
+   const discription =document.getElementById("discription") ;
+   const fxhr = new FXHR();
+   fxhr.open('Post', '');
+   meetinglist = !meetinglist ? [] : meetinglist;
+   let meetingdata = {day: date, hour: time, location: place,data:discription};
+   meetinglist.push(meetingdata);
+   fxhr.send({ meeting: meetinglist[meetinglist.length-1] }, (message) => {
+    alert(message);});
 }
 document.addEventListener('DOMContentLoaded', app.init);
 const select = document.getElementById('select');
 select.addEventListener('change', selectChange());
 
-function selectChange(){
+function selectChange() {
     const month = select.value;
     const body = {
         filter: {
@@ -56,7 +78,7 @@ function selectChange(){
     fxhr.open('GET', '/meetings/filtered');
     const res = fxhr.send(body);
 
-    if (res.status == 200){
+    if (res.status == 200) {
 
     }
 
